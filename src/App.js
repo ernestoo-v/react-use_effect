a791568b1133fc0d97c065f9f64e5bb9fee99de8
Ernesto Villar <evillarperez@gmail.com>
2023-04-10 06:50:04 +0200

    new

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+
+
 
 function App() {
+  const [count, setCount] = useState(0);
+  const [calculation, setCalculation] = useState(0);
+
+  const [count2, setCount2] = useState(0);
+
+
+  useEffect(() => {
+    setCalculation(() => count * 2);
+  }, [count]);
+
+
+  //Will print changed whenever the count2 value changed
+  useEffect(() => {
+    console.log('changed');
+  }, [count2]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <button onClick={() => setCount((c) => c + 1)}>+</button>
+      <p>{calculation}</p>
+      <p>{count2}</p>
+      <button onClick={() => setCount2((a) => a + 1)}>-</button>
+
+
     </div>
   );
 }
-
 export default App;
